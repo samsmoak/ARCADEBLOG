125a47ff832e4d2e31baacb6ec44b054fcc36c2a
samsmoak <45909225+samsmoak@users.noreply.github.com>
2021-11-16 21:36:10 +0530

    mom

--- client/src/components/API.js
+++ client/src/components/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "https://arcadesettlers.herokuapp.com/",
+	baseURL: "https://arcadesettlers.herokuapp.com/api/",
 	// withCredentials: false,
 	// headers: {
 	// 	"Access-Control-Allow-Origin": "*",
