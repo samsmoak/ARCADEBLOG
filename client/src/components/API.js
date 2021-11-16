2d622ffb476c208f9133cd11f36dc4328d3d0a45
samsmoak <45909225+samsmoak@users.noreply.github.com>
2021-11-16 16:17:39 +0530

    makefretimee

--- client/src/components/API.js
+++ client/src/components/API.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default axios.create({
 	baseURL: "https://arcadeblog.herokuapp.com/api/",
-	withCredentials: false,
-	headers: {
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-	},
+	// withCredentials: false,
+	// headers: {
+	// 	"Access-Control-Allow-Origin": "*",
+	// 	"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+	// },
 });
